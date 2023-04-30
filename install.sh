ce98c75d41fd0ce23c2bc604b6e2b6249bea5b4b
gy741 <gy741.kim@gmail.com>
2023-04-30 18:24:18 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+sudo fallocate -l 4G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
 sudo apt-get update
 sudo apt-get install python3 python3-pip -y
 
