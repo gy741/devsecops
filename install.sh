72e04f282c8843be03fe3d584f7cba5fd4cb7748
gy741 <gy741.kim@gmail.com>
2023-04-30 15:55:09 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# gitleaks 설치
+wget https://github.com/gitleaks/gitleaks/releases/download/v8.16.3/gitleaks_8.16.3_linux_x64.tar.gz
+tar -xzf gitleaks_8.16.3_linux_x64.tar.gz
+sudo mv gitleaks /usr/local/bin/
+rm -rf gitleaks_8.16.3_linux_x64.tar.gz
+
+# 노출 키 테스트 저장소
+git clone https://github.com/trufflesecurity/test_keys
